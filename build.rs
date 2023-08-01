9ef6328ed8b6adafd4a242a92d10f4fdf5a9bae9
liuhu016 <liuhu016@realsee.com>
2023-08-01 11:23:32 +0800

    slm login

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-use std::{env, path::PathBuf};
 
 fn main() {
     println!("hello, build.rs");
@@ -11,24 +10,4 @@ fn main() {
         &"/usr/local/share/senseshield/sdk/C/lib64"
     );
     println!("cargo:rustc-link-lib=static={}", library_name);
-    /*
-    println!("cargo:rerun-if-changed=ss_lm_runtime.h");
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("ss_lm_runtime.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-    */
 }
